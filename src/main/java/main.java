a55cb7126d52048486d5524e6c3a31969ab14e21
Adam Doliński <a.m.p.dolinski@gmail.com>
2019-10-20 19:40:02 +0200

    update of the task2 (ENG)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Podaj liczbę, do jakiej mam wyświetlić listę: ");
+//        System.out.println("Podaj liczbę, do jakiej mam wyświetlić listę: ");
+        System.out.println("Enter the size (as number) of how long list of consecutive numbers you want me to show you: ");
+
         Scanner scanner = new Scanner(System.in);
 
-        int liczba = Integer.parseInt(scanner.nextLine());
+        int sizeOfList = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= liczba; i++) {
+        for (int i = 1; i <= sizeOfList; i++) {
             System.out.println(i);
         }
     }
